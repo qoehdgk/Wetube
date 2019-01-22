a0f7a7857415ee8933c08ef65c30598360979def
BaeDongHa <coww1996@naver.com>
2019-01-23 02:19:38 +0900

    Controller add C of MVC

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,7 +1,28 @@
 import express from "express";
+import routes from "../routes";
 
 const userRouter = express.Router();
 
-export default userRouter;
+userRouter.get(routes.users, (req, res) => res.send("Users"));
+userRouter.get(routes.userDetail, (req, res) => res.send("User Detail"));
+userRouter.get(routes.editProfile, (req, res) => res.send("Edit Profile"));
+userRouter.get(routes.changePassword, (req, res) =>
+  res.send("Change Password")
+);
 
+export default userRouter;
 // MVC       DATABASE , TEMPLEATE , FUNCTION
+
+// SameThing
+
+/*
+function alalal() {
+    return true;
+}
+
+alalaldd = () => {
+    return true;
+}
+
+aldkfjadflj = () => true;
+*/
