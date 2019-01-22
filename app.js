b2c445b9f2b0bfc8507b27f3e2af6893fc7abe82
BaeDongHa <coww1996@naver.com>
2019-01-22 23:59:42 +0900

    Seperate Router, export

--- app.js
+++ app.js
@@ -3,13 +3,9 @@ import morgan from "morgan"; //log
 import helmet from "helmet"; //Security
 import cookieParser from "cookie-parser"; //Save User data to Cookie
 import bodyParser from "body-parser"; //Get Object from body
+import { userRouter } from "./router"; //Just const userRouter export
 
 const app = express();
-
-const PORT = 4000;
-
-const handleListening = () =>
-  console.log(`Listening on: http://localhost:${PORT}`);
 
 const handleHome = (req, res) => res.send("Hello from Mine");
 
@@ -25,4 +21,6 @@ app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening);
+app.use("/user", userRouter); //if request User , Go userRouter /
+
+export default app;
